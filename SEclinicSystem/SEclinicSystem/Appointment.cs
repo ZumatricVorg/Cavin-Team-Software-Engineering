198e533e545c6f44f5cb9d74be1b23d2d6ebeba9
elden <elden@mailer.blueoceanit.com.my>
2017-11-13 13:46:43 -0800

    elden's testing

--- SEclinicSystem/SEclinicSystem/Appointment.cs
+++ SEclinicSystem/SEclinicSystem/Appointment.cs
@@ -8,13 +8,46 @@ namespace SEclinicSystem
 {
     class Appointment
     {
-        private DateTime dateTime;
+        const string Cstate = "complete";
+        private string inputTime;
+        private DateTime inputDate = new DateTime();
         private string patientName;
         private string gpName;
+        OverSurgerySystem dbCon = new OverSurgerySystem();
+        int result;
 
-        private bool book()
+        public void setDate(DateTime d)
         {
-            return true;
+            this.inputDate = d;
+        }
+
+        public DateTime getDate()
+        {
+            return this.inputDate;
+        }
+
+        public void setTime(string t)
+        {
+            this.inputTime = t;
+        }
+
+        public string getTime()
+        {
+            return this.inputTime;
+        }
+
+        public bool book(string id, string time,DateTime date,string remark)
+        {
+            result = dbCon.WriteData("INSERT INTO Appointment(staffID, startTime, startDate, Cstatus, remark) VALUES('"+id+"', '" + time + "','" + date + "', 'pending', '" + remark + "')");
+
+            if (result == 1)
+            {
+                return true;
+            }else
+            {
+                return false;
+            }
+           
         }
 
         private bool change()
@@ -22,9 +55,11 @@ namespace SEclinicSystem
             return true;
         }
 
-        private void checke()
+        public void check(string id, DateTime date, string time)
         {
-
+            
+            result = dbCon.getLocalSQLDataCount("SELECT COUNT(*) FROM Appointment WHERE staffID = '"+id+"' AND Cstatus != '"+Cstate+"' AND startTime = '"+time+"' AND startDate = '"+date+"'");
+            //SELECT staffID, startTime, startDate FROM Appointment WHERE staffID = '1' AND Cstatus != 'completed' AND startTime = '10:20:00' AND startDate = '2017-09-11';
         }
 
         private void cancel()
---
+++ SEclinicSystem/SEclinicSystem/AppointmentMain.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEclinicSystem
+{
+    public partial class AppointmentMain : Form
+    {
+        Appointment appointment = new Appointment();
+
+        public AppointmentMain()
+        {
+            InitializeComponent();
+            gpNames();
+            fillTime();
+            fillRemark();
+        }
+
+        public void gpNames()
+        {
+            gpName.Items.Add("doctor1");
+            gpName.SelectedIndex = 0;
+        }
+
+        public void fillTime()
+        {
+      
+            var start = DateTime.ParseExact("09:00", "HH:mm",CultureInfo.InvariantCulture);
+            var clockQuery = from offset in Enumerable.Range(0,37)
+                             select start.AddMinutes(20 * offset);
+
+            foreach (var time in clockQuery)
+                timeList.Items.Add(time.ToString("hh:mm tt"));
+
+            timeList.SelectedIndex = 0;
+        }
+
+        public void fillRemark()
+        {         
+            remark.Items.Add("none");
+            remark.Items.Add("immediate");
+            remark.Items.Add("others");
+            remark.SelectedIndex = 0;
+        }
+
+        private void createApt_Click(object sender, EventArgs e)
+        {
+            string remarkText = remark.Text;
+            appointment.setDate(date.Value);
+            appointment.setTime(timeList.SelectedItem.ToString());
+
+            if (remarkText == "immediate")
+            {
+                // timeList.SelectedItem + "','" + date.Value.ToShortDateString() + "', 'pending', '" + remark.SelectedItem
+               
+                appointment.book("1",appointment.getTime(), appointment.getDate().Date, remarkText);
+                
+            }
+            else
+            {
+                TimeSpan toTime = TimeSpan.Parse(appointment.getTime());
+                DateTime aptTime = appointment.getDate() + toTime;
+                //aptTime = aptTime + appointment.getDate().Date;
+                int checkTime = DateTime.Compare(aptTime,DateTime.Now);
+                //int checkDate = DateTime.Compare(appointment.getDate().Date,now.Date);         
+                //if aptTime > now = 1
+                //if aptTime < now = -1
+                //if aptTime == now = 0 
+
+                if(checkTime >= 0)
+                {
+                    appointment.check("1", appointment.getDate().Date, appointment.getTime());
+
+                }else
+                {
+                    MessageBox.Show("Pass time is not valid");
+                }
+
+            }
+        }
+    }
+}
